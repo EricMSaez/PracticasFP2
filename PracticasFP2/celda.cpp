013477875bb3f572ef836028512464760fca58f2
EricMSaez <EricMSaez@hotmail.com>
2025-02-26 01:36:32 +0100

    Creacion modulo Celda

--- PracticasFP2/celda.cpp
+++ PracticasFP2/celda.cpp
@@ -1,1 +1,75 @@
 #include "celda.h"
+
+void inicializar(tCelda Celda) {
+	Celda.visible = false;
+	Celda.estado = VACIA;
+	Celda.numero = 0;
+	Celda.marcada = false;
+}
+
+tEstado dame_estado(tCelda Celda) {
+	tEstado estadoCelda;
+	estadoCelda = Celda.estado;
+	return estadoCelda;
+}
+
+int dame_numero(tCelda Celda) {
+	int numeroCelda;
+	numeroCelda = Celda.numero;
+	return numeroCelda;
+}
+
+bool es_visible(tCelda Celda) {
+	bool visibilidadCelda;
+	visibilidadCelda = Celda.visible;
+	return visibilidadCelda;
+
+}
+
+bool es_mina(tCelda Celda) {
+	bool esMina=false;
+	if (Celda.estado == MINA) esMina = true;
+	return esMina;
+}
+
+bool esta_vacia(tCelda Celda) {
+	bool estaVacia = false;
+	if (Celda.estado == VACIA) estaVacia = true;
+	return estaVacia;
+}
+
+bool contiene_numero(tCelda Celda) {
+	bool contieneNumero = false;
+	if (Celda.estado == NUMERO) contieneNumero = true;
+	return contieneNumero;
+}
+
+bool esta_marcada(tCelda Celda) {
+	bool estaMarcada = false;
+	if (Celda.marcada) estaMarcada = true;
+	return estaMarcada;
+}
+
+void descubrir_celda(tCelda Celda) {
+	Celda.visible = true;
+}
+
+void ocultar_celda(tCelda Celda) {
+	Celda.visible = false;
+}
+
+void poner_mina(tCelda Celda) {
+	Celda.estado = MINA;
+}
+
+void marcar_mina(tCelda Celda) {
+	Celda.marcada = true;
+}
+
+void desmarcar_celda(tCelda Celda) {
+	Celda.marcada = false;
+}
+
+void poner_numero(tCelda Celda, int num) {
+	Celda.numero = num;
+}
--- PracticasFP2/celda.h
+++ PracticasFP2/celda.h
@@ -1,8 +1,11 @@
-#ifndef celda_h
+#ifndef celda_h //Proteccion de inclusiones duplicadas
 #define celda_h
 
 #include <iostream>
 using namespace std;
+
+
+// Declaracion de tipos
 
 typedef enum { NUMERO, VACIA, MINA } tEstado;
 
@@ -13,10 +16,37 @@ typedef struct {
 	bool marcada;
 } tCelda;
 
+
+
+// Declaración de las funciones
+
 void inicializar(tCelda Celda);
 
 tEstado dame_estado(tCelda Celda);
 
 int dame_numero(tCelda Celda);
+
+bool es_visible(tCelda Celda);
+
+bool es_mina(tCelda Celda);
+
+bool esta_vacia(tCelda Celda);
+
+bool contiene_numero(tCelda Celda);
+
+bool esta_marcada(tCelda Celda);
+
+void descubrir_celda(tCelda Celda);
+
+void ocultar_celda(tCelda Celda);
+
+void poner_mina(tCelda Celda);
+
+void marcar_mina(tCelda Celda);
+
+void desmarcar_celda(tCelda Celda);
+
+void poner_numero(tCelda Celda, int num);
+
 
 #endif
