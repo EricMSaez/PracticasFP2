a43d7ecccb47becf074f518b086329ab97723079
EricMSaez <EricMSaez@hotmail.com>
2025-03-08 13:45:12 +0100

    Cambios para Testing

--- PracticasFP2/main.cpp
+++ PracticasFP2/main.cpp
@@ -3,6 +3,11 @@
 int main() {
 	cout << "Ole Ole" << endl;
 	tJuego juego;
+	tListaPosiciones listaPos;
 	cargar_juego(juego);
+	inicializar_listaPosiciones(listaPos);
+	mostrar_juego_consola(juego.tableroJuego);
+	juega(juego, 0, 3, listaPos);
+	cout << endl;
 	mostrar_juego_consola(juego.tableroJuego);
 }
