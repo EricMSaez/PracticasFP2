1ab3c4e05eaf1dffc377fd5d1f9fd44da0045d4f
Violeta Garcia <viioletagarciia15@gmail.com>
2025-03-09 13:10:41 +0100

    Cambios main (falta "Undo")

--- PracticasFP2/main.cpp
+++ PracticasFP2/main.cpp
@@ -1,13 +1,38 @@
 #include "allHeaders.h"
+using namespace std;
 
 int main() {
 	cout << "Ole Ole" << endl;
 	tJuego juego;
 	tListaPosiciones listaPos;
-	cargar_juego(juego);
-	inicializar_listaPosiciones(listaPos);
-	mostrar_juego_consola(juego.tableroJuego);
-	juega(juego, 0, 3, listaPos);
-	cout << endl;
-	mostrar_juego_consola(juego.tableroJuego);
+	int fila, columna;
+
+	if (cargar_juego(juego)) {
+		mostrar_cabecera();
+		pedir_pos(fila, columna); //Pide la posicion al jugador y la guarda
+		inicializar_listaPosiciones(listaPos);
+		mostrar_juego_consola(juego.tableroJuego);
+
+		while (fila != -1 && columna != -1) { //Si fila = -1 y columna = -1, el juego termina
+
+			if (fila == -2 && columna == -2) { //Si fila = -2 y columna = -2, se marca la celda que indique el jugador  
+				cout << "MARCAR/DESMARCAR mina:" << endl;
+				pedir_pos(fila, columna);
+				marcar_desmarcar(juego, fila, columna);
+				mostrar_juego_consola(juego.tableroJuego);
+			}
+			else if (fila == -3 && columna == -3) { //Si fila = -3 y columna = -3, se realiza "undo"
+
+			}
+			else {
+				juega(juego, fila, columna, listaPos);
+				mostrar_juego_consola(juego.tableroJuego);
+			}
+			pedir_pos(fila, columna);
+		}
+		mostrar_resultado(juego);
+
+	}
+	else cout << "Error. No se ha podido cargar el archivo";
+
 }
