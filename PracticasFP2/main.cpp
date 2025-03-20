6cf2e15786d8913e2d86e04bb2b0459fb2c8abb2
EricMSaez <EricMSaez@hotmail.com>
2025-03-20 19:51:44 +0100

    Quitar comentarios

--- PracticasFP2/main.cpp
+++ PracticasFP2/main.cpp
@@ -63,9 +63,6 @@ void marcarDesmarcar(tJuego& juego) {
 void undoJugada(tJuego& juego, tListaUndo& listaUndo) {
 	tListaPosiciones listaPos;
 	listaPos = ultimos_movimientos(listaUndo);
-	//cout << listaUndo.cont;
-	//cout << listaPos.cont;
-	
 	for (int i = 0; i < listaPos.cont; i++) {
 		int x = dame_posX(listaPos, i);
 		int y = dame_posY(listaPos, i);
