a945f3f686b40f356bc8c0269cd348887db1950c
EricMSaez <151865051+EricMSaez@users.noreply.github.com>
2025-03-20 16:32:09 +0100

    Cambios Undo y Juega

--- PracticasFP2/listaUndo.cpp
+++ PracticasFP2/listaUndo.cpp
@@ -1,16 +1,17 @@
 #include "listaUndo.h"
+#include "tablero.h"
 
 void inicializar(tListaUndo& lista_undo) {
 	lista_undo.cont = 0;
 }
 
-void insertar_final(tListaUndo& lista_undo, tListaPosiciones& lista_pos) {
-
-	if (lista_undo.cont == MAX_UNDO) {
-		for (int i = 0; i < MAX_UNDO - 1; i++) {
+void insertar_final(tListaUndo& lista_undo, tListaPosiciones& lista_pos, tTablero tablero) {
+	if (lista_undo.cont == MAX_UNDO) {		//Desplaza todas las listas de posiciones en caso de que este llena la lista undo
+		for (int i = 0; i < MAX_UNDO; i++) {
 			lista_undo.lista[i] = lista_undo.lista[i + 1];
 		}
-		lista_undo.lista[MAX_UNDO - 1] = lista_pos;
+		lista_undo.lista[0] = lista_pos;
+		lista_undo.cont++;
 	}
 	else {
 		lista_undo.lista[lista_undo.cont] = lista_pos;
@@ -21,9 +22,9 @@ void insertar_final(tListaUndo& lista_undo, tListaPosiciones& lista_pos) {
 
 tListaPosiciones ultimos_movimientos(tListaUndo lista_undo) {
 
-	tListaPosiciones posicion;
+	tListaPosiciones ultimaPosicion;
 	int final = lista_undo.cont;
-	posicion = lista_undo.lista[final - 1];
+	ultimaPosicion = lista_undo.lista[final - 1];
 
-	return posicion;
+	return ultimaPosicion;
 }
