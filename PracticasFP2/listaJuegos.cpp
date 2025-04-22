f3961df41fb9432a48c331a40dee736eab7fe45d
EricMSaez <EricMSaez@hotmail.com>
2025-04-23 00:13:48 +0200

    Funciones listaJuegos

--- PracticasFP2/listaJuegos.cpp
+++ PracticasFP2/listaJuegos.cpp
@@ -1,17 +1,61 @@
 #include "listaJuegos.h"
 
 void inicializar(tListaJuegos& lista_juegos) {
-	
+	lista_juegos.lista = new tJuego*[MAX_JUEGOS];
+	lista_juegos.capacidad = MAX_JUEGOS;
+	lista_juegos.cont = 0;
 }
 
-void destruye(tListaJuegos& tlista_juegos);
+void destruye(tListaJuegos& lista_juegos) {
+		while (lista_juegos.cont >= 0) {
+		delete lista_juegos.lista[lista_juegos.cont];
+		lista_juegos.cont--;
+		lista_juegos.capacidad++;
+	}
 
-void insertar(const tListaJuegos& lista_juegos, const tJuego& juego);
+	delete [] lista_juegos.lista;
+}
 
-int numero_juegos(const tListaJuegos& lista_juegos);
+void insertar(tListaJuegos lista_juegos, const tJuego& juego) {
+	if (lista_juegos.capacidad > 0) {
+		lista_juegos.lista[lista_juegos.cont] = new tJuego;
+		lista_juegos.lista[lista_juegos.cont]->mina_explotada = juego.mina_explotada;
+		lista_juegos.lista[lista_juegos.cont]->num_descubiertas = juego.num_descubiertas;
+		lista_juegos.lista[lista_juegos.cont]->num_jugadas = juego.num_jugadas;
+		lista_juegos.lista[lista_juegos.cont]->num_minas = juego.num_minas;
+		lista_juegos.lista[lista_juegos.cont]->tableroJuego = juego.tableroJuego;
 
-bool es_vacia(const tListaJuegos& lista_juegos);
+		lista_juegos.capacidad--;
+		lista_juegos.cont++;
+	}
+}
 
-tJuego dame_juego(const tListaJuegos& lista_juegos, const int& pos);
+int numero_juegos(const tListaJuegos& lista_juegos) {
+	int numero_juegos;
+	numero_juegos = lista_juegos.cont;
+	return numero_juegos;
+}
 
-void eliminar(const tListaJuegos& lista_juegos, const int& pos);
+bool es_vacia(const tListaJuegos& lista_juegos) {
+	bool es_vacia = true;
+	
+	if (lista_juegos.cont > 0) {
+		es_vacia = false;
+	}
+	return es_vacia;
+}
+
+tJuego dame_juego(const tListaJuegos& lista_juegos, const int& pos) {
+	tJuego juego;
+	juego = *lista_juegos.lista[pos];
+	return juego;
+}
+
+void eliminar(tListaJuegos lista_juegos, const int& pos) {
+	int contador=pos;
+	while (contador < lista_juegos.cont) {
+		lista_juegos.lista[contador] = lista_juegos.lista[contador + 1];
+		contador++;
+	}
+	delete lista_juegos.lista[lista_juegos.cont];
+}
